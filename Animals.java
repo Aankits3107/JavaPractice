6b8881a8f8731e988fde679d0f9aa361b05c1791
Aankits3107 <ankits3107@gmail.com>
2022-10-09 16:11:29 +0530

    Abstract classes & Interfaces

---
+++ Animals.java
@@ -0,0 +1,9 @@
+public abstract class Animals {
+    int age;
+    String name;
+    public abstract void makeNoise();
+
+    public void printName() {
+        System.out.println("My name is: " + name);
+    }
+}
---
+++ Cat.java
@@ -0,0 +1,9 @@
+public class Cat extends Janvar implements AnimalsStuff {
+    public void makeNoise() {
+        System.out.println("Meaow");
+    }
+    @Override
+    public void poop() {
+        System.out.println("Phlbblobpblop");        
+    }
+}
