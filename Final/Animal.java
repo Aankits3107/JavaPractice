74de356a5b54253b5366ffb909dac835dfe6711d
Aankits3107 <ankits3107@gmail.com>
2022-10-18 16:12:14 +0530

    Final Keyword

---
+++ Final/Animal.java
@@ -0,0 +1,10 @@
+package Final;
+
+public class Animal {     
+    public void eat() {
+        System.out.println("muunch");
+    }
+    public void makeNoise() {
+        System.out.println("Hello, I am an Animal.");
+    }
+}
---
+++ Final/FinalExample.java
@@ -0,0 +1,17 @@
+//Final - in class - prevents other class to be a subclass of the patent class.
+//  Class not to be extended b y other classes.
+//Final - in method - a sublass can't override a method which is final in the parent class.
+//Final - as variable - a variable that is assigned only one time.
+//  To create constants. Constants are declared at the start of the code right below the class declaration and outside any method.
+//  Constant name are in upper cases if single word and for multi word seprated by underscore.
+
+package Final;
+
+public class FinalExample {
+    public static final double PI = 3.14159;
+    public static void main(String[] args) {
+        final Dog myDog;
+        myDog = new Dog();
+        myDog.eat(2);        
+    }
+}
