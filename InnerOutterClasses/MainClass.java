4f1f61441ed283f180a3d95f371848bec6f7399b
Aankits3107 <ankits3107@gmail.com>
2022-10-18 16:12:30 +0530

    Inner and Outter Classes

---
+++ InnerOutterClasses/MainClass.java
@@ -0,0 +1,16 @@
+//Inner classes are also called nested classes. A class which is defined inside a class.
+//can have data types, methods, classes.
+//Used in very larger applications. In case where a certain were class that only ever make sence anyweher only in side of another class.
+
+package InnerOutterClasses;
+
+public class MainClass {
+    public static void main(String[] args) {
+        OutterClass outter = new OutterClass();
+            outter.heyThere();
+        OutterClass.InnerClassNonStatic innerNonStatic = outter.new InnerClassNonStatic(); //to call the inner class of the outter class, we have to call the constructor to make a inner class variable.
+            innerNonStatic.WhatsUp();
+        OutterClass.InnerClassStatic innerStatic = new OutterClass.InnerClassStatic(); //to call the inner class of the outter class, we have to call the constructor to make a inner class variable.
+            innerStatic.WhatsUp();
+    }
+}
