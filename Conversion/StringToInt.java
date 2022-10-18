cfac94d292974c25e494b2868da69750afcca546
Aankits3107 <ankits3107@gmail.com>
2022-10-18 16:11:44 +0530

    Conversion

---
+++ Conversion/StringToInt.java
@@ -0,0 +1,10 @@
+package Conversion;
+
+public class StringToInt {
+    public static void main(String[] args) {
+        String sI = "23";
+            System.out.println(sI+6); //string appended. adding a string at end of it.
+        int total = Integer.parseInt(sI); //string conversion to integer.
+            System.out.println(total + 6);
+    }
+}
