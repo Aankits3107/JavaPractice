169075a5a0d464b67b94741ee1841767117484c2
Aankits3107 <ankits3107@gmail.com>
2022-10-18 13:59:05 +0530

    Lambda

--- Lambda/Cat.java
+++ Lambda/Cat.java
@@ -1,12 +1,12 @@
 package Lambda;
 
-public class Cat implements Printable {
-    public String name;
-    public int age;
+// public class Cat implements Printable {
+//     public String name;
+//     public int age;
 
-    public Cat() {
-    }
-    public void print() {
-        System.out.println("Meow");
-    }
-}
+//     public Cat() {
+//     }
+//     public void print() {
+//         System.out.println("Meow");
+//     }
+// }
