190069094e626b31f98db39af30481c967e9df6a
Aankits3107 <ankits3107@gmail.com>
2022-10-29 13:04:21 +0530

    If Else

---
+++ IfStatement/If.java
@@ -0,0 +1,25 @@
+//to perform a block og code if it's condition evaluate to be ture.
+//very basic statement for decision making
+// > -greater than , < -lesser than , == -comparing two object --Comparision operator
+//= -seting a value --asssigning operator
+
+package IfStatement;
+
+public class If {
+    public static void main(String[] args) {
+        int age = 75;
+
+        if(age  >= 75) {
+            System.out.println("OK Boomer");
+        }
+        else if(age >= 18) {
+            System.out.println("You are an Adult!");
+        }
+        else if(age >= 13) {
+            System.out.println("You are an Teenager!");
+        }
+        else {
+            System.out.println("You are not an Adult!");
+        }
+    }
+}
