ed11db308319a3b106405f30c6cc71bd5b50cfba
Aankits3107 <ankits3107@gmail.com>
2022-11-05 14:19:29 +0530

    Overloaded constructor

--- Constructors/OverloadedConstructors.java
+++ Constructors/OverloadedConstructors.java
@@ -5,6 +5,16 @@ package Constructors;
 
 public class OverloadedConstructors {
     public static void main(String[] args) {
-        
+        Pizza pizza1 = new Pizza("Thin crust", "Tamato", "Mozzerella", "Peperroni");
+            System.out.println("Here are the ingredients of your pizza 1: Bread- " + pizza1.braed + ", Sauce-  " + pizza1.sauce + ", Cheese-  " + pizza1.cheese + ", Topping- " + pizza1.topping);
+    
+        Pizza pizza2 = new Pizza("Thick crust", "Tamato", "Mozzerella");
+            System.out.println("Here are the ingredients of your pizza 2: Bread- " + pizza2.braed + ", Sauce- " + pizza2.sauce + ", Cheese- " + pizza2.cheese);
+
+        Pizza pizza3 = new Pizza("Cheeseburst", "Tamato");
+            System.out.println("Here are the ingredients of your pizza 3: Bread- " + pizza3.braed + ", Sauce- " + pizza3.sauce);
+
+        Pizza pizza4 = new Pizza("Plain");
+            System.out.println("Here are the ingredients of your pizza 4: Bread- " + pizza4.braed);
     }
 }
--- Constructors/Pizza.java
+++ Constructors/Pizza.java
@@ -1,5 +1,31 @@
 package Constructors;
 
 public class Pizza {
-    
+
+    String braed;
+    String sauce;
+    String cheese;
+    String topping;
+
+    Pizza(String bread, String sauce, String cheese, String topping) {
+        this.braed = bread;
+        this.sauce = sauce;
+        this.cheese = cheese;
+        this.topping = topping;
+    }
+
+    Pizza(String bread, String sauce, String cheese) {
+        this.braed = bread;
+        this.sauce = sauce;
+        this.cheese = cheese;
+    }
+
+    Pizza(String bread, String sauce) {
+        this.braed = bread;
+        this.sauce = sauce;
+    }
+
+    Pizza(String bread) {
+        this.braed = bread;
+    }
 }
