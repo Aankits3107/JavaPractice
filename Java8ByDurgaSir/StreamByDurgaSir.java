38e08ca7657de86835c6f99aab56347d92381827
Aankits3107 <ankits3107@gmail.com>
2022-11-22 19:51:23 +0530

    Stream By Durga Sir

--- Java8ByDurgaSir/StreamByDurgaSir.java
+++ Java8ByDurgaSir/StreamByDurgaSir.java
@@ -21,14 +21,22 @@
 //                                  returns -ve if obj1 has to come after obj2 / obj2 has to come after obj1
 //                                  returns 0 if obj1 & obj2 are equal
 //                          Desending - (i1,i2) -> (i1 < i2) ? 1 : (i1 > i2) ? -1 : 0
-//          min() & max() method - 
-//  If you want to be objects, process objects from collection the we should go for stream.
+//                          Comparator() - uses lambda expression, customised sorting order
+//          min() method - to find the min elements in a integer list, according to a comparator.
+//          max() method - to find the max elements in a integer list, according to a comparator.
+// To get array elemenst we can use - 
+//                          forEach() method - always requires some function (functional interface so, we can send lambda expresion in here) to perform a required activity. For example if we need to access all element of a list. or to print all of them etc.
+//                          toArray() method - to convert stream of objects/elements into arrays.
+//                          Stream.of() method - to get a stream of an array[].
+//                                              Wheres there is a group of elements or arrays are then we can go for stream.of concept.
+//  If you want objects, process objects from collection the we should go for stream.
 //  Best advantage of stream/lambda - very consise code (short code/less no of line code).
 
 package Java8ByDurgaSir;
 
 import java.util.*;
-import java.util.stream.*;
+import java.util.stream.*; // whenever we are importring a package, all classes & interface are present by default but not sub-level packages classes & interfaces. So we need to access the sub-package level by importing it through main package..
+import java.util.function.*; // sub-level package.
 
 public class StreamByDurgaSir {
     public static void main(String[] args) {
@@ -45,7 +53,7 @@ public class StreamByDurgaSir {
         myList.add(6);
         myList.add(10);
             System.out.println("Marks list: " + myList);
-        //filter
+        //filter()
         List<Integer> EvenMarks = myList.stream().filter(i -> i%2 == 0).collect(Collectors.toList()); //.filter('predicate condition') predicate is a boolen and it is functional interface, and function interface can be replaced with lambda expresion. Then we have to collect the data by a collector method in to a list.  
             System.out.println("Even no Marks: " + EvenMarks);
 
@@ -55,21 +63,22 @@ public class StreamByDurgaSir {
         long noOfFailedStudent = myList.stream().filter(i -> i < 4).count(); // to count the no of failed student. No to for count() be collect to a list.
             System.out.println("Total Less then passing marks: " + noOfFailedStudent);
 
-        //map
+        //map()
         List<Integer> updateMarks = myList.stream().map(i -> i+5).collect(Collectors.toList());
             System.out.println("5 Garce Marks:" + updateMarks);
 
-        //sort
+        //sort()
         //sorting in interger list
         //asending
         List<Integer> sortedMarksListAsending = myList.stream().sorted().collect(Collectors.toList()); //natural sorted order
         // List<Integer> sortedMarksListAsending = myList.stream().sorted((i1, i2) -> (i1.compareTo(i2))).collect(Collectors.toList());
+        // List<Integer> sortedMarksListAsending = myList.stream().sorted((i1, i2) -> -(i2.compareTo(i1))).collect(Collectors.toList());
             System.out.println("Asending Sorted marks list: " + sortedMarksListAsending);
 
         //desending
         List<Integer> sortedMarksListDesending = myList.stream().sorted((i1, i2) -> ((i1 < i2) ? 1 : (i1 > i2) ? -1 : 0)).collect(Collectors.toList()); //coustom sorted order - desending order by comparetor order.
-        // List<Integer> sortedMarksListDesending = myList.stream().sorted((i1, i2) -> -(i1.compareTo(i2))).collect(Collectors.toList()); // can ve done like this also by a -ve compareTo()
-        // List<Integer> sortedMarksListDesending = myList.stream().sorted((i1, i2) -> (i2.compareTo(i1))).collect(Collectors.toList()); // can ve done like this also by a reverse compareTo(), which is -ve onle, bcz reverse order means -ve
+        // List<Integer> sortedMarksListDesending = myList.stream().sorted((i1, i2) -> -(i1.compareTo(i2))).collect(Collectors.toList()); // can be done like this also by a -ve compareTo()
+        // List<Integer> sortedMarksListDesending = myList.stream().sorted((i1, i2) -> (i2.compareTo(i1))).collect(Collectors.toList()); // can be done like this also by a reverse compareTo(), which is -ve only, bcz reverse order means -ve
             System.out.println("Decending Sorted marks list: " + sortedMarksListDesending);
         
         //Sorting in string list
@@ -103,5 +112,47 @@ public class StreamByDurgaSir {
                                             };
         List<String> increasingLength = myStringList.stream().sorted(comp).collect(Collectors.toList()); //alphabatical order is not consider.
             System.out.println("Order by increasing character length: " + increasingLength);
+    
+        //min()
+        Integer minValue = myList.stream().min((i1, i2) -> i1.compareTo(i2)).get();
+        // Integer minValue = myList.stream().min((i1, i2) -> -i1.compareTo(i2)).get(); //reverse of min.
+            System.out.println("Min value: " + minValue);
+
+        //max()
+        Integer maxValue = myList.stream().max((i1, i2) -> i1.compareTo(i2)).get();
+        // Integer maxValue = myList.stream().max((i1, i2) -> -i1.compareTo(i2)).get(); //reverse of max
+            System.out.println("Max value: " + maxValue);
+        
+        //forEach()
+        myList.stream().forEach(System.out::println); // '::' constructor reference
+        myStringList.stream().forEach(System.out::println);
+
+        //custom function for forEach()
+        Consumer<Integer> con = i -> {
+                                        System.out.println("The square of " + i + " is " + (i * i));
+                                    };
+        myList.stream().forEach(con);
+
+        //toArray()
+        Integer[] myInteger = myList.stream().toArray(Integer[] :: new); // '::' constructor reference, here giving new reference variable by it.
+        for(Integer i1 : myInteger) {
+                System.out.println("Integer Array: " + i1);
+        }
+
+        String[] myString = myStringList.stream().toArray(String[] :: new); // '::' constructor reference, here giving new reference variable by it.
+        for(String s1 : myString) {
+                System.out.println("String Array: " + s1);
+
+        //Stream.of()
+        Stream.of(myInteger).forEach(System.out::println);
+        Stream.of(myString).forEach(System.out::println);
+
+        Stream<Integer> myStream = Stream.of(9, 99, 999, 9999, 99999);
+            myStream.forEach(System.out::println);
+            System.out.println(myStream); // retruns the address in memory
+
+        Integer[] i = {10, 20, 30, 40, 50};
+        Stream.of(i).forEach(System.out::println);
+        }
     }
 }
