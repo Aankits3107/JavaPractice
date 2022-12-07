e91e85edbb828cbc20ce0c69285a2ca0acaf6c8d
Aankits3107 <ankits3107@gmail.com>
2022-12-07 15:45:40 +0530

    Update Interfaces.java

--- Interface/Interfaces.java
+++ Interface/Interfaces.java
@@ -1,5 +1,5 @@
 //You can implements as many interfaces as you can, but can't extends more then one abstract class.
-//Every filed which defined in an interface is static and final. can't change their vales. In abstarct you can change, and they can have therre own values.
+//Every feiled which defined in an interface is static and final. can't change their vales. In abstarct you can change, and they can have therre own values.
 //Instead of extends interfaces are implemented. When you implements an inteface then you have to implents the methods from that interface.
 //Interfaces - If you have a lot of unrelated classes, that you all want to do a certain thing that makes it so you can garantee that other types of classes will be perform the task if they aren't in a class.
 
