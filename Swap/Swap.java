53f0c9ce1dd1f25311f834b59cc2bea0256c8ee5
Aankits3107 <ankits3107@gmail.com>
2023-04-08 12:12:56 +0530

    Update Swap.java

--- Swap/Swap.java
+++ Swap/Swap.java
@@ -14,3 +14,24 @@ public class Swap {
             System.out.println("Y: " +y);
     }
 }
+
+
+// public class ArraySwap {
+//     public static void main(String[] args) {
+//         char[] arr = {'a', 'b', 'c', 'd'};
+//         char temp;
+        
+//         // Swap b,a and c,d
+//         temp = arr[0];
+//         arr[0] = arr[1];
+//         arr[1] = temp;
+//         temp = arr[2];
+//         arr[2] = arr[3];
+//         arr[3] = temp;
+        
+//         // Print result in reverse order
+//         for (int i = arr.length - 1; i >= 0; i--) {
+//             System.out.print(arr[i] + " ");
+//         }
+//     }
+// }
